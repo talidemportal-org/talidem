c66932b52f31ab682433fdfc3525f7f6573dde23
talidemportal-org <talidemportal@gmail.com>
2026-06-13 01:49:03 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#39FF14'
+      },
+      fontFamily: {
+        gaming: ['Orbitron', 'sans-serif']
+      }
+    }
+  }
+}
